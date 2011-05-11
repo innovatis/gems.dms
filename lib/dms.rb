0a826df689e7e633fa9a0e0bd2d2199dca30ed8b
Burke Libbey <burke@burkelibbey.org>
2011-05-11 11:29:16 -0500

    fix

--- lib/dms.rb
+++ lib/dms.rb
@@ -1,10 +1,22 @@
 require 'json'
 
 module DMS
-  def self.location
-    "http://dms.innovatisinc.ca"
-  end 
   
+  def self.location
+    if Rails.env.development?
+      "http://dms.dev"
+    else
+      "http://dms.innovatisinc.ca"
+    end
+  end
+  
+  def self.url_for_private(path, expires_in = 1.hour)
+    RestClient.post("#{location}/keys", {
+        :path => path,
+        :secret => DMS.secret,
+        :expires_in => expires_in})
+  end
+
   def self.access_urls(paths, expires_in = 1.hour)
     app_id = Rails.application.config.inno_app_id
     secret = Rails.application.config.inno_app_secret
